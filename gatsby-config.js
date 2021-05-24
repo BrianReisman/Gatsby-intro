fe1f3d3bc7c9947fe213fc174921874f5c67208a
BrianReisman <brianreisman@protonmail.com>
2021-05-24 19:26:36 -0400

    create usePost custom hook and render posts to home

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,23 @@ module.exports = {
     description:
       "A site build during a full-day Frontend Master's Gatsby workshop",
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: 'posts',
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Minnesota!</p>
-    <Link to="/about">Learn about me &rarr;</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Minnesota!</p>
+      <Link to="/about">Learn about me &rarr;</Link>
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <pre>{JSON.stringify(post, null, 2)}</pre>
+      ))}
+    </Layout>
+  );
+};
